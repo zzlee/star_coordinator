cfa50cb1cacbc4e7cf052e7199330784962b81f8
kaiser <kaiser@feltmeng.com>
2013-09-14 11:38:31 +0800

    add trace to sendRequestToRemot and port change to 80

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ var globalConnectionMgr = require('./global_connection_mgr.js');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
