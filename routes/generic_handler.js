b286e8dc830a8e5dd6399592a9cd38d37f42fcb8
dev <dev@feltmeng.com>
2013-09-23 21:23:34 +0800

    Added some traces in "GET /internal/ugc_serial_no"

--- routes/generic_handler.js
+++ routes/generic_handler.js
@@ -3,8 +3,10 @@ var genericHandler = {};
 var ugcSerialNoMgr = require('../ugc_serial_no_mgr.js');
 
 genericHandler.cbOfGetUgcSerialNo = function(req, res) {
+	logger.info('[GET '+req.path+'] is called');
     var ugcSerialNo = ugcSerialNoMgr.getUgcSerialNo(function(err, ugcSerialNo){
-        res.send({ugcSerialNo: ugcSerialNo});
+    	logger.info('[GET '+req.path+'] ugcSerialNo='+ugcSerialNo);
+    	res.send({ugcSerialNo: ugcSerialNo});
     });
 };
 
